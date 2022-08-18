e888318ace2f0fc94b175b8ab7e95d02b9a41750
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2022-08-18 13:16:39 -0300

    Initial commit.

---
+++ src/components/ContadorConHook.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { useCounter } from '../hooks/useCounter';
+
+export const ContadorConHook = () => {
+    const { valor, acumular } = useCounter(5);
+    return (
+        <>
+            <h3>Contador <small>{valor}</small></h3>
+            <button className='btn btn-primary' onClick={() => acumular(1)}>+1</button>
+            &nbsp;
+            <button className='btn btn-primary' onClick={() => acumular(-1)}>-1</button>
+        </>
+    )
+}
---
+++ src/typescript/TiposBasicos.tsx
@@ -0,0 +1,16 @@
+export const TiposBasicos = () => {
+    const nombre: string = 'Fernando';
+    const edad: number = 35;
+    const estaActivo: boolean = true;
+
+    const poderes: string[] = []; //'Velocidad', 'Volar', 'Respirar en el agua'
+
+    return (
+        <>
+            <h3>Tipos básicos</h3>
+            {nombre}, {edad}, {estaActivo ? 'Activo' : 'No activo'}
+            <br />
+            {poderes.join(', ')}
+        </>
+    )
+}
