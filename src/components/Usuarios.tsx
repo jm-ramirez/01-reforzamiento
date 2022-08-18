cde9a565fbd6580e0fe0864ca6cc23f53ff37189
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2022-08-18 16:27:44 -0300

    Request http with axios.

---
+++ src/components/Usuarios.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { reqResApi } from '../api/reqRes';
+
+export const Usuarios = () => {
+    useEffect(() => {
+        //Llamada al API
+        reqResApi.get('/users').then(resp => {
+            console.log(resp.data.data);
+        }).catch(console.log)
+    }, [])
+
+    return (
+        <>
+            <h3>Usuarios</h3>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Avatar</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </>
+    )
+}
